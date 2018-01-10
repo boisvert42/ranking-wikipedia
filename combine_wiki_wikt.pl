2812f67ed6be85d5f8d29bace3c2f98d9b76151b
boisvert42 <boisvert42@gmail.com>
2018-01-10 10:56:00 -0800

    Create combine_wiki_wikt.pl

---
+++ combine_wiki_wikt.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $wiki_file = $ARGV[0];
+my $wikt_file = $ARGV[1];
+my @files = ($wiki_file, $wikt_file);
+
+my %dict;
+for my $file (@files)
+{
+    open FILE, $file or die $!;
+    while (<FILE>)
+    {
+        chomp;
+        my ($word,$score) = ($_ =~ /^(.*)@(.*)$/);
+        if ($dict{$word})
+        {
+            $dict{$word} = $score if $score > $dict{$word};
+        }
+        else {$dict{$word} = $score;}
+    }
+    close FILE;
+}
+
+open OUTFILE, '>RankedWikiWikt.txt' or die $!;
+foreach (sort { ($dict{$b} <=> $dict{$a}) || ($a cmp $b) } keys %dict)
+{
+    print OUTFILE $_ . "\@" . $dict{$_} . "\n";
+}
+
+close OUTFILE;
