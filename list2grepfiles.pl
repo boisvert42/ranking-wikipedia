48c42e7c77ece355b4c9150f04134cc4f7f3c9c7
boisvert42 <boisvert42@gmail.com>
2018-01-10 11:32:26 -0800

    Create list2grepfiles.pl

---
+++ list2grepfiles.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Data::Dumper;
+
+my $rankedwikifile = $ARGV[0];
+my $rankedwikiwiktfile = $ARGV[1];
+
+my %files = ($rankedwikifile => 'Wiki.txt',
+            $rankedwikiwiktfile => 'WikiWikt.txt'
+            );
+
+while (my ($f,$g) = each(%files))
+{
+    open FILE, $f or die $!;
+    open OUTFILE, ">$g" or die $!;
+    while (<FILE>)
+    {
+        my ($orig,$score) = ($_ =~ /^(.*)@(.*)$/);
+        last if $score < 30;
+        (my $tx = $orig) =~ s/[^A-Za-z0-9]//g;
+        print OUTFILE "$tx\%$orig\@$score\n";
+    }
+    close FILE;
+    close OUTFILE;
+}
